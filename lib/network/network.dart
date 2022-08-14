459a8cc3be9625da20e13872e06f8fcdabdbbf79
Ahmadsaniliman <saniahmadliman@gmail.com>
2022-08-14 22:20:12 +0100

    problem with the api

--- lib/network/network.dart
+++ lib/network/network.dart
@@ -2,15 +2,24 @@ import 'dart:convert';
 
 import 'package:http/http.dart';
 import 'package:newsapp/model/model.dart';
-import 'package:newsapp/util/util.dart';
+// import 'package:newsapp/util/util.dart';
+import 'dart:developer' as devtool show log;
 
 class Network {
   Future<NewsModel> getNews() async {
-    final url =
-        'https://newsapi.org/v2/everything?q=tesla&from=2022-07-11&sortBy=publishedAt&apiKey=${Util.apiKey}';
+    const url =
+        'https://newsapi.org/v2/everything?q=tesla&from=2022-07-11&sortBy=publishedAt&apiKey=b786bc4553bb45a3b45ba1b3a2c8377b';
 
-    final Response response = await get(Uri.parse(url));
+    final response = await get(
+      Uri.parse(
+        Uri.encodeFull(url),
+      ),
+    );
     if (response.statusCode == 200) {
+      devtool.log(url);
+      devtool.log(
+        Uri.encodeFull(response.body),
+      );
       return NewsModel.fromJson(
         json.decode(response.body),
       );
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/model/model.dart';
+import 'package:newsapp/network/network.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,8 +10,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late final Future<NewsModel> newsOnject;
+
+  @override
+  void initState() {
+    newsOnject = Network().getNews();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('News '),
+      ),
+      body: FutureBuilder(
+        future: newsOnject,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const Center(
+              child: Text('All Good'),
+            );
+          } else if (snapshot.hasError) {
+            return Text('Error : ${snapshot.error}');
+          } else {
+            return const CircularProgressIndicator();
+          }
+        },
+      ),
+    );
   }
 }
